8f02c2e0467359a8f6c60ab8dad3d86bed57647b
Alex Everett <aeverett4746@gmail.com>
2023-10-15 18:40:25 -0600

    Added docstrings to module

--- read_origin.py
+++ read_origin.py
@@ -1,15 +1,54 @@
 #! /usr/bin/env python3
 
+"""
+A module that finds all instances of a regular expression in a file
+"""
+
 import re
 import sys
 
 def transform_into_regexpress(input_string):
+
+    """
+    Takes string and in sequence turns it into a raw string and then into a regular expression object
+
+    Parameters
+    ----------
+    input_string: str
+        String from user input (later from command line)
+
+    Returns
+    -------
+    re_expression_obj: obj
+        Regular expression pattern object
+    """
+
     expression = r'{}'.format(input_string)
     re_expression_obj = re.compile(expression)
     return re_expression_obj
 
 
 def find_in_file(input_file, output_file, input_string):
+    """
+    Finds all instances of an expression, from a user input string, in a text file
+    Writes instances of expression and line they are found in into output text file
+
+    Parameters
+    ----------
+    input_file: str
+        Filepath for file to be read
+
+    output_file: str
+        Filepath for where instances fo expression should be written to
+
+    input_string: str
+        Input string to be turned into regular expression pattern object
+
+    Returns
+    -------
+    None
+    """
+
     expression = transform_into_regexpress(input_string)
     line_number = 0
     with open(input_file, "r") as infile, open(output_file, "w") as outfile:
